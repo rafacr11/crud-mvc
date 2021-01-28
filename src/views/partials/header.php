e94c865f3604cf748e4f330d1923c7ed8c569f88
Rafael Ribeiro <rcr.rafa1@gmail.com>
2021-01-27 22:00:30 -0300

    Atualizacao

---
+++ src/views/partials/header.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+    <title> Meu CRUD em MVC </title>
+    <link rel="stylesheet" href="<?=$base;?>/assets/css/style.css" >
+</head>
+
+<body>
+    <header>
+        <h1>Título do site</h1>
+        <hr>
+    </header>
